5d3fecf67086fc5fdf482c3f90d2d3823277b632
Matthew Lockdall <mlockdall26@gmail.com>
2020-04-30 15:45:00 -0500

    ring css cleaned up (#3)
    Co-authored-by: Matthew Lockdall <matthewlockdall@US00571.local>

--- src/components/Ring.js
+++ src/components/Ring.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { useRingActions } from './../utilHooks/index';
 import { useDrag } from 'react-dnd';
+import Store from './../contexts/Store';
 
 const Ring = ({ color, ringNumber, location }) => {
+    const { ring } = useContext(Store);
     const previousIndex = location; // find ring id then find last intem in that ring aray
     const [ringData, drag] = useDrag({
         item: { type: 'ring', ringNumber, previousIndex },
@@ -15,14 +17,14 @@ const Ring = ({ color, ringNumber, location }) => {
     const { setActiveRing } = useRingActions();
     const { isDragging, didDrop } = ringData;
 
-    // console.log('ringData', ringData);
-
-    const [pos, setpos] = useState({ top: '', left: '' });
-    // console.log('drag', drag);
+    // const [pos, setpos] = useState({ top: '', left: '' });
 
     useEffect(() => {
         if (didDrop) {
-            setpos({ ...pos, top: -12, left: -12 });
+            if (ring.current === ringNumber) {
+                const ringNow = document.getElementById(ringNumber);
+                ringNow.classList.add('onBoard');
+            }
         }
         return () => {};
     }, [didDrop]);
@@ -34,15 +36,8 @@ const Ring = ({ color, ringNumber, location }) => {
             className={`ring ${ringNumber}`}
             onMouseOver={(e) => setActiveRing(e, color)}
             style={{
-                position: 'absolute',
-                top: `${pos.top}px`,
-                left: `${pos.left}px`,
-                width: '24px',
-                height: '24px',
                 border: `3px solid ${color}`,
-                borderRadius: '50%',
-                backgroundColor: 'transparent',
-                zIndex: 4,
+
                 boxShadow: ` 0 0 8px  ${isDragging ? 'green' : 'transparent'} `,
             }}
         ></div>
